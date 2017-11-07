15b4dace1363f279ab3f16da64f05005aff1605a
tungvt45 <tungvt2167@gmail.com>
2017-11-07 21:36:47 +0700

    test2

--- DemoGit/src/demogit/DemoGit.java
+++ DemoGit/src/demogit/DemoGit.java
@@ -27,8 +27,9 @@ public class DemoGit {
         
         System.out.println("AHIHIHHIHIHIH");
         
-        System.out.println("S?a thêm ??t nè haha");
+        System.out.println("S?a thï¿½m ??t nï¿½ haha");
         System.out.println("thá»­ láº¡i nÃ¨ :D");
+        System.out.println("AHUHU :(((");
     }
     
 }
